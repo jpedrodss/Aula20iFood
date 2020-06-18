c9a6e363e794b0d704492bf36a8a546b12b3f724
João Pedro <jpedrodss@gmail.com>
2020-06-18 12:23:55 -0300

    função pedir concluida

--- Pedido.cs
+++ Pedido.cs
@@ -1,31 +1,53 @@
 using System;
 namespace Aula20iFood
 {
-    public class Pedido : Restaurante
+    public class Pedido
     {
-        public string[] Itens = new string[2];
+        public string[] Itens = new string[3];
         public string Cliente { get; set; }
         public string Restaurante { get; set; }
         public string FormaDePGTO { get; set; }
         public bool PedidoPago { get; set; }
         public int contadorItens = 0;
 
+        public void Pedir(){
+            try{
+                System.Console.WriteLine("Deseja fazer um novo pedido? (y = sim / n = não)");
+                    string resposta = Console.ReadLine();
+                    if(resposta == "y"){
+                        do{
+                                System.Console.WriteLine("Qual item você gostaria de pedir?");
+                                Itens[contadorItens] = Console.ReadLine();
+                                contadorItens++;
+                        }while(contadorItens < Itens.Length);
+                        if (contadorItens >= Itens.Length) {
+                            System.Console.WriteLine("\nLimite de pedido alcançado.\n");
+                        }
+
+                        System.Console.WriteLine($"Confirmar o pedido com os seguintes itens? (y = sim / n = nao)\n -{Itens[0]}\n -{Itens[1]}\n -{Itens[2]}");
+                        string confirmacao = Console.ReadLine();
+
+                        if(confirmacao == "y"){
+                            System.Console.WriteLine($"Pedido concluido em {DateTime.Now}.\nItens pedidos: {Itens[0]}, {Itens[1]} e {Itens[2]}.");
+                        }else if(confirmacao == "n"){
+                            contadorItens = 0;
+                            Itens[0] = null;
+                            Itens[1] = null;
+                            Itens[2] = null;
+                            Pedir();
+                        }
+                    } else if (resposta == "n"){
+                        System.Console.WriteLine("Pedido cancelado.");
+                    } else {
+                        System.Console.WriteLine("Insira um comando válido.");
+                    }
+            }catch (Exception e){
+                System.Console.WriteLine(e);
+            }
+        }
 
         public string EntregarPedido(){
             return $"Seu pedido do cliente {Cliente}, foi entregue.";
-        }
-        public void Pedir(){
-        System.Console.WriteLine("Deseja fazer um novo pedido?");
-            string resposta = Console.ReadLine();
-            if(resposta == "y"){
-                if(contadorItens < Itens.Length){
-                    System.Console.WriteLine("Qual Item você gostaria de pedir?");
-                    Itens[contadorItens] = Console.ReadLine();
-                    contadorItens++;
-                }else{
-                    System.Console.WriteLine("Limite de pedido cheio.");
-                }
-            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,16 +7,22 @@ namespace Aula20iFood
         static void Main(string[] args)
         {
 
+            Pedido pedido = new Pedido();
+            Cliente clt = new Cliente();
             Restaurante rest = new Restaurante();
+            
             rest.Endereco = "Av. Guarulhos, 123";
+            pedido.Restaurante = rest.NomeFantasia;
             rest.NomeFantasia = "Straight Up Arroz";
 
-            Cliente clt = new Cliente();
-            clt.Nome = "João";
+            
+            clt.Nome = Console.ReadLine();
+            pedido.Cliente = clt.Nome;
             clt.EnderecoAtual = "R. do Bamburral, 597";
 
-            Pedido pedido = new Pedido();
+            pedido.Pedir();
             
+
         }
     }
 }
